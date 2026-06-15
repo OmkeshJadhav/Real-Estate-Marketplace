3c7dbd0dff28fe96ce5e53ea49d754486e12f70f
Omkesh <omkeshjadhav.dev@gmail.com>
2026-06-15 22:40:29 +0530

    Added pages and routing - Sign up, sign in, home, about, profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <>
-      <h1 className="text-red-500 font-bold text-4xl">
-        
-        
-        
-        Hello World</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/sign-up' element={<SignUp/>}/>
+          <Route path='/sign-in' element={<SignIn/>}/>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
