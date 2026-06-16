6afb356bf9094f525583f94787401ebbc9415940
Omkesh <omkeshjadhav.dev@gmail.com>
2026-06-16 22:34:27 +0530

    Feat: Added mongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import { log } from "node:console";
+dotenv.config();
 
-const PORT = 3000
+const PORT = process.env.PORT
 const app = express()
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log('Failed to connect to mongoDB', err)
+    }
+    )
 
 app.use(express.json())
 
