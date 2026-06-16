34cdaeb3a517b4ff0958ce3ba37a554db0679efa
Omkesh <omkeshjadhav.dev@gmail.com>
2026-06-16 17:08:04 +0530

    feat: Added header/navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header/>
         <Routes>
           <Route path='/sign-up' element={<SignUp/>}/>
           <Route path='/sign-in' element={<SignIn/>}/>
