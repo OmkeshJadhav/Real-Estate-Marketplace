78c806abfa98b7d73cab6695ab01e418e52f1651
Omkesh <omkeshjadhav.dev@gmail.com>
2026-06-17 08:32:57 +0530

    feat: Password hasing using bcrypt

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,20 +1,36 @@
 import globalErrorHandler from "../middlewares/globalErrorHandler.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const signUp = asyncHandler(async (req, res) => {
     const { username, email, password } = req.body
 
-    const newUser = new User({ username, email, password })
+    // Check if user already exists in db
+    const isExistingUser = await User.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    if (isExistingUser) {
+        return res.status(400).json({
+            success: false,
+            message: 'Username or email already exists! Please try with different username or email id.'
+        })
+    }
+
+    const salt = await bcrypt.genSalt(12)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const newUser = new User({ username, email, password:hashedPassword })
     await newUser.save()
 
     res.status(201).json({
         success: true,
         message: "User registered successfully.",
         data: {
-            // _id: user._id,
+            _id: newUser._id,
             username,
-            email
+            email,
         }
     })
 })
