0a93fd6072e3605ecd7956498e00d525a5a6be8b
Omkesh <omkeshjadhav.dev@gmail.com>
2026-06-21 10:28:11 +0530

    feat: Sign in user with jwt

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -2,7 +2,8 @@ import globalErrorHandler from "../middlewares/globalErrorHandler.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-import {errorHandler} from "../utils/errorHandler.js"
+import { errorHandler } from "../utils/errorHandler.js"
+import { generateAccessToken } from "../utils/generateAccessToken.js";
 
 export const signUp = asyncHandler(async (req, res, next) => {
     try {
@@ -39,4 +40,50 @@ export const signUp = asyncHandler(async (req, res, next) => {
         next(error)
         // next(errorHandler(550, 'Error from the function'))
     }
+})
+
+export const signIn = asyncHandler(async (req, res) => {
+
+    const { identifier, password } = req.body;
+
+    const user = await User.findOne({
+        $or: [{ email: identifier }, { username: identifier }]
+    })
+
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: `Invalid credentialsssss.`
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
+        return res.status(401).json({
+            success: false,
+            message: `Invalid credentials`
+        })
+    }
+
+    const accessToken = generateAccessToken(user)
+
+    // res.cookie is more secure than storing JWT in localStorage using res.json - because JavaScript cannot access an httpOnly cookie.
+    res
+        .cookie("accessToken", accessToken, {
+            httpOnly: true,
+            maxAge: 10 * 60 * 1000
+            // secure: process.env.NODE_ENV === "production",
+            // sameSite: "strict",
+        })
+        .status(200)
+        .json({
+            success: true,
+            message: `User logged in successfully.`,
+            data: {
+                _id: user._id,
+                username: user.username,
+                email: user.email
+            },
+        })
 })
---
+++ server/utils/generateAccessToken.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken"
+
+export const generateAccessToken = (user) => {
+    return jwt.sign(
+        {
+            userId: user._id,
+            email: user.email
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "10m"
+        }
+    );
+};
